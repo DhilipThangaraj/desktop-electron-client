9af3baa0ef61b1f025bacbe8a7b497b6e43e5646
Dhilipkumar Thangaraj <dhilipthangaraj777@gmail.com>
2022-10-06 17:56:48 +0400

    about window customization

--- image-resizer-electron/main.js
+++ image-resizer-electron/main.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const { app, BrowserWindow, Menu } = require("electron");
 
-const isDev = process.env.NODE_DEV !== "production";
-const isWindow = process.platform !== "win32";
+const isDev = process.env.NODE_DEV !== "development";
+const isWindow = process.platform === "win32";
 
 //create the main window
 function createMainWindow() {
@@ -17,21 +17,39 @@ function createMainWindow() {
   if (isDev) {
     mainWindow.webContents.openDevTools();
   }
-  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+//create the about window
+function createAboutWindow() {
+  //Instantiating the browser window - whenever the app launches that time under the hood electron client initiate chromium browser.
+  const aboutWindow = new BrowserWindow({
+    title: "About Image Resizer",
+    width: 300,
+    height: 300,
+  });
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
 }
 
 //Menu Template
 const menu = [
   {
-    label: "File",
-    submenu: [
-      {
-        label: "Quit",
-        click: () => app.quit(),
-        accelerator: "ctrl+w",
-      },
-    ],
+    role: "fileMenu",
   },
+  ...(isWindow
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
 ];
 
 //When App is ready
